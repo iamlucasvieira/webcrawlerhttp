457d1d45ea4b090dee2c441f064783cada4146f4
Lucas Vieira dos Santos <lucas6eng@gmail.com>
2024-07-07 17:58:59 +0200

    Add page crawling

--- crawl.js
+++ crawl.js
@@ -1,11 +1,41 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+    console.log("🕷️ Crawling:", currentURL);
+
+    try {
+        const resp = await fetch(currentURL);
+
+        if (!resp.ok) {
+            console.error("❌ Error:", resp.statusText);
+            return;
+        }
+
+        const contentType = resp.headers.get("content-type");
+        if (!contentType.includes("text/html")) {
+            console.error("❌ Error: Not an HTML page");
+            return;
+        }
+
+        console.log(await resp.text());
+    } catch (e) {
+        console.error("❌ Error:", e.message);
+        return;
+    }
+
+    // console.log("🔍 Status:", resp.status);
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = [];
     const dom = new JSDOM(htmlBody);
     dom.window.document.querySelectorAll("a").forEach((a) => {
-        const url = new URL(a.href, baseURL);
-        urls.push(url.href);
+        try {
+            const url = new URL(a.href, baseURL);
+            urls.push(url.href);
+        } catch (e) {
+            console.error("❌ Invalid URL:", a.href);
+        }
     });
     return urls;
 }
@@ -22,4 +52,5 @@ function normalizeURL(urlString) {
 module.exports = {
     normalizeURL,
     getURLsFromHTML,
+    crawlPage,
 };
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+const { crawlPage } = require("./crawl.js");
+
+function main() {
+    const expectedLength = 3;
+    const actualLength = process.argv.length;
+    if (actualLength !== expectedLength) {
+        console.log("Usage: node main.js <website-url>");
+        console.error(
+            `\n😢 Expected ${expectedLength - 2} argument, but got ${actualLength - 2}.`,
+        );
+        process.exit(1);
+    }
+
+    const url = process.argv[2];
+    console.log("Crawling website:", url);
+    crawlPage(url);
+}
+
+main();
